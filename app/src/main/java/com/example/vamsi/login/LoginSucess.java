d7fa7e697d493e2e57840e0827beb7acfe03b164
vetjurv4 <vetjurv4@gmail.com>
2020-09-13 07:07:35 +0200

    added firebase and firestore

--- app/src/main/java/com/example/vamsi/login/LoginSucess.java
+++ app/src/main/java/com/example/vamsi/login/LoginSucess.java
@@ -1,6 +1,6 @@
 package com.example.vamsi.login;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginSucess extends AppCompatActivity {
