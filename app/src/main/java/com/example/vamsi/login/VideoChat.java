ef860dde508e94265648c31c3b92b4b89703970b
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-14 15:11:12 +0200

    fixed bug

--- app/src/main/java/com/example/vamsi/login/VideoChat.java
+++ app/src/main/java/com/example/vamsi/login/VideoChat.java
@@ -6,41 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 
-//import com.vidyo.VidyoClient.Connector.ConnectorPkg;
-//import com.vidyo.VidyoClient.Connector.Connector;
+import com.vidyo.VidyoClient.Connector.ConnectorPkg;
+import com.vidyo.VidyoClient.Connector.Connector;
 
-//public class VideoChat extends AppCompatActivity implements Connector.IConnect {
-//
-//    private Connector vc;
-//    private FrameLayout videoFrame;
-//    private String const apiToken = "cHJvdmlzaW9uAHNlZ2F1bHRAY2U5ODAyLnZpZHlvLmlvADYzNzY3Mzk0ODkxAAAxYTA1MDRhNTQ2MmRjMDhlMjhlMDg4MjZmMTNhNWJlZWY2OTc0NjAzMTcxYTNmN2ZiZTA4NjczYmMyNGY2ZTE2ZWZhNTk3NjliZDYwZTUyY2ZiZGI3NWMwYzAyZWFiOTk=";
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_video_chat);
-//
-//        ConnectorPkg.setApplicationUIContext(this);
-//        ConnectorPkg.initialize();
-//        videoFrame = (FrameLayout)findViewById(R.id.videoFrame);
-//    }
-//
-//    public void Start(View v) {
-//        vc = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 2, "r", "", 0);
-//        vc.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
-//    }
-//
-//    public void Connect(View v) {
-//        vc.connect("prod.vidyo.io", apiToken, 'DemoUser' , "DemoRoom", this);
-//    }
-//
-//    public void Disconnect(View v) {
-//        vc.disconnect();
-//    }
-//
-//    public void onSuccess() {}
-//
-//    public void onFailure(Connector.ConnectorFailReason reason) {}
-//
-//    public void onDisconnected(Connector.ConnectorDisconnectReason reason) {}
-//}
+public class VideoChat extends AppCompatActivity implements Connector.IConnect {
+
+    private Connector vc;
+    private FrameLayout videoFrame;
+    private String apiToken = "cHJvdmlzaW9uAHNlZ2F1bHRAY2U5ODAyLnZpZHlvLmlvADYzNzY3Mzk0ODkxAAAxYTA1MDRhNTQ2MmRjMDhlMjhlMDg4MjZmMTNhNWJlZWY2OTc0NjAzMTcxYTNmN2ZiZTA4NjczYmMyNGY2ZTE2ZWZhNTk3NjliZDYwZTUyY2ZiZGI3NWMwYzAyZWFiOTk=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
+
+        ConnectorPkg.setApplicationUIContext(this);
+        ConnectorPkg.initialize();
+        videoFrame = (FrameLayout)findViewById(R.id.videoFrame);
+    }
+
+    public void Start(View v) {
+        vc = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 2, "r", "", 0);
+        vc.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
+    }
+
+    public void Connect(View v) {
+        vc.connect("prod.vidyo.io", apiToken, "DemoUser" , "DemoRoom", this);
+    }
+
+    public void Disconnect(View v) {
+        vc.disconnect();
+    }
+
+    public void onSuccess() {}
+
+    public void onFailure(Connector.ConnectorFailReason reason) {}
+
+    public void onDisconnected(Connector.ConnectorDisconnectReason reason) {}
+}
